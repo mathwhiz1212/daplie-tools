8c5e99965987dfde74d272fcc6ec7899e8291f64
AJ ONeal <awesome@coolaj86.com>
2016-04-09 00:31:30 -0600

    add basic test for bash cli

---
+++ test-basic-cli.sh
@@ -0,0 +1,6 @@
+node bin/daplie accounts:list
+node bin/daplie addresses:list
+node bin/daplie devices:list
+node bin/daplie dns:list --all
+node bin/daplie domains:list
+node bin/daplie wallet:sources:list
